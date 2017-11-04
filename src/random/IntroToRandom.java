01b984dbe61482911138d264676e736eaf8856b9
Declimator <league@WorthyMini-5.local>
2017-11-03 18:04:15 -0700

    RandomIntro, Dice Roll, RockPaperScissors

--- src/random/IntroToRandom.java
+++ src/random/IntroToRandom.java
@@ -11,20 +11,23 @@ import java.util.Random;
 
 public class IntroToRandom {
 	public static void main(String[] args) {
-		int r = 0;
+
 		
 		//2. Now make r random. 
 		//   Create an object of the Random class
 		//   Hint: new Random().nextInt()
 
+int r = new Random().nextInt(100);
 		//3. Limit the random number between 0 and 100
-
+int b = new Random().nextInt(51) + 25;
 		//4. Limit the random number between 25 and 75. Hint: (highest value - lowest value) + lowest value
-
+int a = new Random().nextInt(301) - 222;
 		//5. Challenge: Limit the random number between -222 and 88
 
 
 		
 		System.out.println(r); 
+		System.out.println(b);
+		System.out.println(a);
 	}
 }
